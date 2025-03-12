30726e824efb4649de3f4a0ab72ff95b03fa41e0
Inhumannn <thomas.pnbm@gmail.com>
2025-03-12 12:23:50 +0100

    fix(product): correct product display and description
    - Fixed product description display issue.
    - Improved data fetching and handling.
    - Updated product page layout.
    - Created cart page.
    - Added basic styling to cart page.

--- front/js/product.js
+++ front/js/product.js
@@ -1,32 +1,33 @@
 async function init(){
    const products = await getDatas();
-   // displayProducts(products)
-   console.log(products)
+   console.log("Valeur id :",products)
+   displayProducts(products)  
 };
 init();
+let products = []
 async function getDatas(){
    try{
       const params = new URL(document.location).searchParams;
-      const id = params.get("id")
-      console.log(id);
-      const req = await fetch(params);
+      const id = params.get("id")   
+      const req = await fetch(`http://localhost:3000/api/products/${id}`);
       return await req.json();
    }catch(error){
       console.error(error);
+      return null;
    };
-}
-// function displayProducts(products){
-//    products.forEach((el) =>{
-//       const{
-//          _id,
-//          titre,
-//          description,
-//          image,
-//          declinaisons
-//       } = el;
-//       document.querySelector('#titre').textContent = titre;
-//       document.querySelector('#description').textContent = description;
-//       document.querySelector('#image').textContent = image;
-//       document.querySelector('.declinaison').textContent = declinaisons;
-//    })
-// };
+};
+function displayProducts(products){
+      const{
+         titre,
+         description,
+         image,
+         declinaisons   
+      } = products;
+      const descriptionCourte = description.split("\n")[0];
+      const descriptionBr = description.split("\n").join("<br>");
+      document.querySelector('#titre').textContent = titre;
+      document.querySelector('#description1').textContent = descriptionCourte;
+      document.querySelector('#description2').innerHTML = descriptionBr;
+      document.querySelector('#image').src = image;
+      document.querySelector('.declinaison').textContent = declinaisons;
+};
