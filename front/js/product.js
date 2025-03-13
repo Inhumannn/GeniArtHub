4d0347e8aa5ac362ee99ce15169f69f48bcce244
Inhumannn <thomas.pnbm@gmail.com>
2025-03-13 12:28:22 +0100

    feat(product): add product to cart functionality
    - Added functionality to add products to cart.
    - Implemented localStorage for cart data.
    - Displays selected product price.
    - Handles quantity selection.
    - Improved input field validation.

--- front/js/product.js
+++ front/js/product.js
@@ -1,10 +1,10 @@
+let products
 async function init(){
-   const products = await getDatas();
-   console.log("Valeur id :", products)
-   displayProducts(products)  
+   product = await getDatas();
+   displayProducts(product)  
 };
 init();
-const products = []
+
 async function getDatas(){
    try{
       const params = new URL(document.location).searchParams;
@@ -35,29 +35,33 @@ function displayProducts(products){
             <option data-price="${el.prix}" value="${el.taille}">${el.taille}</option>
             `);
          });
+      const firstOption = select.options[select.selectedIndex]; 
+      document.querySelector('.showprice').textContent = `${firstOption.dataset.price}€`;
       select.addEventListener('change', function(el){
          const option = el.target.options[el.target.selectedIndex];
          document.querySelector('.showprice').textContent = `${option.dataset.price}€`;
-        
       });
 };
-// localStorge.setItem(key, value); // Ajout
-// const ... localStorage.getItem('...'); // Afficher
-// localStorage.removeItem('...'); // Supression
-// const user = {nom: "Dupont", prenom: "Jacks"}; // 1. Stocker
-// localStorage.setItem("user", JSON.stringify(user)); // 2. Sérialisation
-// const monObjet = localStorage.getItem("user"); // 3. Désérialiser
-// const monObjetParse = JSON.parse(monObjet); // 3. Désérialiser
-// const{om, prenom} = monObjetParse; // 4. Détructuration
-// console.log(nom, prenom); // Affichage dans ma console
 const btn_buy = document.querySelector('.button-buy');
 btn_buy.addEventListener('click', (e) => {
-   e.preventDefault().
-   console.log('blabal')
+   e.preventDefault();
+   const formatSelectionne = document.querySelector('#format').value;
+   const quantiteSelectionnee = document.querySelector('#quantity').value || 1;
+   const {_id, image, shorttitle} = product;
+   const cart2 = [{_id, image, shorttitle, format:formatSelectionne, quantite:quantiteSelectionnee}];
+   let cart = JSON.parse(localStorage.getItem("cart"))
+   // Faire un find(index) sur mon cart (localStorage)
+   cart.find()
+   if(cart){
+      cart.push(cart2);
+   }else{
+      cart = [cart2];
+   };
+   localStorage.setItem("cart", JSON.stringify(cart));
+   const monObjet = localStorage.getItem('cart');
+   const monobjetParse = JSON.parse(monObjet);
+   console.log(monobjetParse);
 });
-
-
 // Reste à faire : 
-   //  Afficher le prix selon le format
    // Utiliser le localStorage
    // Ensuite faire ma page cart
